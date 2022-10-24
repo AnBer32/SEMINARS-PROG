c04986bb4edcb8692b142bca01d9f50ef3eda08f
AnBer <beryozkinanastya@mail.ru>
2022-10-24 15:58:43 +0300

    Задача 10

---
+++ Seminar2/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine ("Введите трехзначное число");
+
+while (true)
+{
+
+    string numb = Console.ReadLine ();
+    if (numb.Length == 3)
+    {
+        Console.WriteLine (numb [1]);
+
+    }
+    else
+    {
+        Console.WriteLine ("Ошибка!!! ВВедите трехзначное число");
+    }
+}
