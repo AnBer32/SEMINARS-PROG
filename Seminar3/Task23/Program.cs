260e4a953b69786cee81a62937f09e1514059f8d
AnBer <beryozkinanastya@mail.ru>
2022-10-24 23:44:42 +0300

    Задача23

---
+++ Seminar3/Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void PrintSquaresToConsole (int n)
+{
+    for (int i=1;i<=n; i++)
+    {
+        Console.WriteLine (i*i*i);
+    }
+}
+PrintSquaresToConsole (5);
