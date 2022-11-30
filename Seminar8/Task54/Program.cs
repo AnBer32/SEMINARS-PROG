81eda3a899ab120629997262d37441078bbd7308
AnBer <beryozkinanastya@mail.ru>
2022-12-01 01:05:49 +0300

    Задача54

---
+++ Seminar8/Task54/Program.cs
@@ -0,0 +1,62 @@
+﻿//Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int a = InputNumbers("Введите a: ");
+int b = InputNumbers("Введите b: ");
+
+int[,] array = new int[a, b];
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"\n Отсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(4, 41);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
